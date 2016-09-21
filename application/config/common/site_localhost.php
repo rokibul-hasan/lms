01076c177cacc9bc743cd9d9828a2c38b5798083
rokibul <rokibulhasan.356@gmail.com>
2016-09-21 12:51:10 +0600

    startup for lms project

---
+++ application/config/common/site_localhost.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$root=$_SERVER['HTTP_HOST'];
+$config['SITE'] = array(
+    "name" => "DEBENHAMS BANGLADESH",
+    'website' => $root,
+    'logo' => 'jamuna logo.gif');
+$config['SITETITLE'] = 'DEBENHAMS BANGLADESH';
+
+$config['main_sidebar_title'] = "DEBENHAMS <br> BANGLADESH <br> MGMT";
+
+$config['DEVELOPER'] = array(
+    "name" => "Friends IT",
+    'website' => "http://friendsitltd.com/");
+
+
+
+$config['ASSET_FOLDER'] = "asset/";
+$config['ADMIN_THEME'] = 'Admin_theme/AdminLTE/'; //Theme location on view folder
+$config['THEME_ASSET'] = $config['ASSET_FOLDER'] . $config['ADMIN_THEME'];
+$config['LOGIN_THEME'] = 'Login_theme/daily_ui_day1_sign_in/'; //Theme location on view folder
+
+
