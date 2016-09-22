4c9df07e237083e2fad7c614c552f3a3f113d4fc
sonjoy <sonjoy@DESKTOP-4A4VK8B>
2016-09-22 13:23:30 +0600

    book 1.23pm(s)

--- application/controllers/Book.php
+++ application/controllers/Book.php
@@ -29,6 +29,7 @@ class Book extends ci_controller{
          $crud = new grocery_CRUD();
         $crud->set_table('book')
                 ->set_subject('Book')
+                ->display_as('PublisherId','Publisher Name')
                 ->set_relation('PublisherId', 'Publisher', 'PublisherName')
                 ->order_by('BookId','desc');
         $output = $crud->render();
@@ -43,6 +44,12 @@ class Book extends ci_controller{
          $crud = new grocery_CRUD();
         $crud->set_table('bookauthor')
                 ->set_subject('Book Author')
+                ->display_as('BookId','Book Name')
+                ->display_as('AuthorId','Author Name')
+                ->display_as('AuthorTypeId','Author Type')
+                ->set_relation('BookId', 'book', 'Title')
+                ->set_relation('AuthorId', 'author', 'AuthorCorporateName')
+                ->set_relation('AuthorTypeId', 'authortype', 'AuthorType')
                 ->order_by('BookAuthorId','desc');
         $output = $crud->render();
         $data['glosary'] = $output;
@@ -56,6 +63,10 @@ class Book extends ci_controller{
          $crud = new grocery_CRUD();
         $crud->set_table('booksubject')
                 ->set_subject('Book Category')
+                ->display_as('BookId','Book Name')
+                ->display_as('SubjectId','Subject')
+                ->set_relation('BookId', 'book', 'Title')
+                ->set_relation('SubjectId', 'subject', 'Title')
                 ->order_by('SubjectId','desc');
         $output = $crud->render();
         $data['glosary'] = $output;
@@ -66,15 +77,35 @@ class Book extends ci_controller{
     }
     
     
-    public function  bookcopy(){
+    public function  book_copy(){
          $crud = new grocery_CRUD();
         $crud->set_table('bookcopy')
                 ->set_subject('Book Copy')
+                ->display_as('BookId','Book Name')
+                ->display_as('LocationId','Location')
+                ->set_relation('BookId', 'book', 'Title')
+                ->set_relation('LocationId', 'location', 'LocationName')
                 ->order_by('BookCopyId','desc');
         $output = $crud->render();
         $data['glosary'] = $output;
         $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
-        $data['Title'] = 'Book Section';
+        $data['Title'] = 'Book Copy';
+        $data['base_url'] = base_url();
+        $this->load->view($this->config->item('ADMIN_THEME') . 'item', $data);
+    }
+    public function  book_include(){
+         $crud = new grocery_CRUD();
+        $crud->set_table('bookinclude')
+                ->set_subject('Book Include')
+                ->display_as('BookId','Book Name')
+                ->set_relation('BookId', 'book', 'Title')
+                ->display_as('IncludeId','Book Name')
+                ->set_relation('IncludeId', 'include', 'Include')
+                ->order_by('BookIncludeId','desc');
+        $output = $crud->render();
+        $data['glosary'] = $output;
+        $data['theme_asset_url'] = base_url() . $this->config->item('THEME_ASSET');
+        $data['Title'] = 'Book Include';
         $data['base_url'] = base_url();
         $this->load->view($this->config->item('ADMIN_THEME') . 'item', $data);
     }
