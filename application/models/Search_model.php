7cc8eed58ef8108ca30afd6f85e35c3e57a4dcf9
mashfiqcse13 <mashfiqnahid@gmail.com>
2016-10-10 08:05:22 +0600

    Advance search model version 1

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -59,7 +59,8 @@ class Search_model extends CI_Model {
         } else {
             $conditions = "";
         }
-        $sql = "SELECT DISTINCT  `BookId` ,  `ISBN` ,  `Title` ,  `BookKeywords` ,  `YearOfPublication` ,  `PlaceOfPublication`  FROM `view_book_details` $conditions LIMIT $limit_offset , $limit_size";
+        $sql = "SELECT DISTINCT  `BookId` ,  `ISBN` ,  `Title` ,  `BookKeywords` ,
+            `YearOfPublication` ,  `PlaceOfPublication`,`PublisherName`  FROM `view_book_details` $conditions LIMIT $limit_offset , $limit_size";
 //        die($sql);
         $result = $this->db->query($sql)->result();
         if (empty($result)) {
@@ -82,8 +83,31 @@ class Search_model extends CI_Model {
      * @return  array   (db 2D array as object)
      */
 
-    function search_journal($bookTitle, $Publisher, $year, $subject, $limit_offset = 0, $limit_size = 20) {
-        $result = $this->db->get("journal", $limit_size, $limit_offset)->result();
+    function search_journal($journalTitle = null, $Publisher = null, $year = null, $subject = null, $limit_offset = 0, $limit_size = 30) {
+        $conditions = array();
+        if (!empty($journalTitle)) {
+            array_push($conditions, "`Title` LIKE  '%$journalTitle%'");
+        }
+        if (!empty($Publisher)) {
+            $Publisher = implode(",", $Publisher);
+            array_push($conditions, "`PublisherId` IN ($Publisher)");
+        }
+        if (!empty($year)) {
+            array_push($conditions, "`Year` BETWEEN  {$year[0]} AND {$year[1]}");
+        }
+        if (!empty($subject)) {
+            $subject = implode(",", $subject);
+            array_push($conditions, "`SubjectId` IN ($subject)");
+        }
+
+        if (!empty($conditions)) {
+            $conditions = "WHERE " . implode(" AND ", $conditions);
+        } else {
+            $conditions = "";
+        }
+        $sql = "SELECT DISTINCT  `JournalId`, `ISSN`, `Title`, `Year`,`PublisherName`  FROM `view_journal_details` $conditions LIMIT $limit_offset , $limit_size";
+//        die($sql);
+        $result = $this->db->query($sql)->result();
         if (empty($result)) {
             return FALSE;
         } else {
@@ -97,7 +121,7 @@ class Search_model extends CI_Model {
      * This will search report from the db
      * 
      * @param   string  $reportTitle
-     * @param   array  $Organization
+     * @param   array  $Organization    (array of string)
      * @param   array  $year    (array of integer . size 2)   (Ex : array(1999,2016)) (Ex : array(2013,2013))
      * @param   array  $keyword    (array of string)
      * @param   array  $subject    (array of integer)
@@ -105,8 +129,46 @@ class Search_model extends CI_Model {
      * @return  array   (db 2D array as object)
      */
 
-    function search_report($reportTitle, $Organization, $year, $keyword, $subject, $limit_offset = 0, $limit_size = 20) {
-        $result = $this->db->get("report", $limit_size, $limit_offset)->result();
+    function search_report($reportTitle = null, $Organization = null, $year = null, $keywords = null, $subject = null, $limit_offset = 0, $limit_size = 20) {
+        $conditions = array();
+        if (!empty($reportTitle)) {
+            array_push($reportTitle, "`Title` LIKE  '%$reportTitle%'");
+        }
+        if (!empty($Organization)) {
+            $Organization_coditions = array();
+            foreach ($Organizations as $Organization) {
+                array_push($Organization_coditions, "`Keywords` LIKE  '%$Organization%'");
+            }
+            if (!empty($Organization_coditions)) {
+                array_push($conditions, implode(" OR ", $Organization_coditions));
+            }
+        }
+        if (!empty($year)) {
+            array_push($conditions, "`Year` BETWEEN  {$year[0]} AND {$year[1]}");
+        }
+        if (!empty($keywords)) {
+            $keyword_coditions = array();
+            foreach ($keywords as $keyword) {
+                array_push($keyword_coditions, "`Keywords` LIKE  '%$keyword%'");
+            }
+            if (!empty($keyword_coditions)) {
+                array_push($conditions, implode(" OR ", $keyword_coditions));
+            }
+        }
+
+        if (!empty($subject)) {
+            $subject = implode(",", $subject);
+            array_push($conditions, "`SubjectId` IN ($subject)");
+        }
+
+        if (!empty($conditions)) {
+            $conditions = "WHERE " . implode(" AND ", $conditions);
+        } else {
+            $conditions = "";
+        }
+        $sql = "SELECT DISTINCT  `ReportId`, `Title`, `Organization`, `Year`, `Keywords`  FROM `view_report_details` $conditions LIMIT $limit_offset , $limit_size";
+//        die($sql);
+        $result = $this->db->query($sql)->result();
         if (empty($result)) {
             return FALSE;
         } else {
@@ -121,15 +183,48 @@ class Search_model extends CI_Model {
      * 
      * @param   string  $thesisTitle
      * @param   array  $year    (array of integer . size 2)   (Ex : array(1999,2016)) (Ex : array(2013,2013))
-     * @param   array  $department    (array of string)
+     * @param   array  $departments    (array of string)
      * @param   array  $Author    (array of integer)
      * @param   array  $subject    (array of integer)
      * 
      * @return  array   (db 2D array as object)
      */
 
-    function search_thesis($thesisTitle, $year, $department, $Author, $subject, $limit_offset = 0, $limit_size = 20) {
-        $result = $this->db->get("thesis", $limit_size, $limit_offset)->result();
+    function search_thesis($thesisTitle, $year, $departments, $Author, $subject, $limit_offset = 0, $limit_size = 20) {
+        $conditions = array();
+        if (!empty($thesisTitle)) {
+            array_push($conditions, "`Title` LIKE  '%$thesisTitle%'");
+        }
+        if (!empty($year)) {
+            array_push($conditions, "`yearofaward` BETWEEN  {$year[0]} AND {$year[1]}");
+        }
+        if (!empty($departments)) {
+            $department_coditions = array();
+            foreach ($departments as $department) {
+                array_push($department_coditions, "`department` LIKE  '%$department%'");
+            }
+            if (!empty($department_coditions)) {
+                array_push($conditions, implode(" OR ", $department_coditions));
+            }
+        }
+
+        if (!empty($Author)) {
+            $Author = implode(",", $Author);
+            array_push($conditions, "`AuthorId` IN ($Author)");
+        }
+        if (!empty($subject)) {
+            $subject = implode(",", $subject);
+            array_push($conditions, "`SubjectId` IN ($subject)");
+        }
+
+        if (!empty($conditions)) {
+            $conditions = "WHERE " . implode(" AND ", $conditions);
+        } else {
+            $conditions = "";
+        }
+        $sql = "SELECT DISTINCT  `Thesisid`, `Title`, `yearofaward`, `department`  FROM `view_thesis_details` $conditions LIMIT $limit_offset , $limit_size";
+//        die($sql);
+        $result = $this->db->query($sql)->result();
         if (empty($result)) {
             return FALSE;
         } else {
